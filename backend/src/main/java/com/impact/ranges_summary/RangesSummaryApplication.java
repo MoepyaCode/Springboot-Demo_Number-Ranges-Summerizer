21703f24d01fe1ac6eb761f25446f3935eaca3f4
MoepyaCode <kagisomoepya19@gmail.com>
2024-11-11 02:34:31 +0200

    🎉 Project init number-ranges-summerizer

---
+++ backend/src/main/java/com/impact/ranges_summary/RangesSummaryApplication.java
@@ -0,0 +1,13 @@
+package com.impact.ranges_summary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RangesSummaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RangesSummaryApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/impact/ranges_summary/RangesSummaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.impact.ranges_summary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RangesSummaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
