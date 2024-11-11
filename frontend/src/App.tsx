f2141cb086afd23ac1d15deca4aa99f99efeb93a
MoepyaCode <kagisomoepya19@gmail.com>
2024-11-12 00:15:42 +0200

    🎉 Frontend initialization

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+
+function App() {
+
+  return (
+    <main className="min-h-screen overflow-hidden grid place-items-center">
+
+      <section>
+        Number Ranges Summerizer
+      </section>
+
+    </main>
+  )
+}
+
+export default App
